5b0374cf2f5f6a5e4980f06d72ad40ef6c2bf7e4
mohamedMoharek <almharyqm107@gmail.com>
2025-10-20 20:22:49 +0300

    hero without message

--- Dahab/src/middlewares/isAuthorized.middleware.ts
+++ Dahab/src/middlewares/isAuthorized.middleware.ts
@@ -2,15 +2,15 @@ import { RequestHandler } from "express";
 import { Role } from "../models/user-model";
 
 export const isAuthorized = (...roles: Role[]): RequestHandler => {
-    return (req, res, next) => {
-        if (req.user) {
-            if (roles.includes(req.user.role)) {
-                next();
-            } else {
-                res.status(403).json({ message: "Forbidden" });
-            }
-        } else {
-            res.status(401).json({ message: "Unauthorized" });
-        }
-    };
+  return (req, res, next) => {
+    if (req.user) {
+      if (roles.includes(req.user.role)) {
+        next();
+      } else {
+        res.status(403).json({ message: "Forbidden" });
+      }
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  };
 };
