44f1f9feea8298bcbd6949ef72043f168236f9ea
3ZB <mahmoudazaab034@gmail.com>
2025-10-23 18:08:58 +0300

    from 3zb

--- Dahab/express.d.ts
+++ Dahab/express.d.ts
@@ -23,26 +23,26 @@
 
 // export {};
 
-// import type { UserToken } from "./src/interfaces/user-token";
-
-// declare global {
-//   namespace Express {
-//     interface User extends UserToken {}
-//     interface Request {
-//       user?: User;
-//     }
-//   }
-// }
 import type { UserToken } from "./src/interfaces/user-token";
-import { IUser } from "./src/models/user-model";
 
 declare global {
   namespace Express {
-    interface User extends IUser {} 
+    interface User extends UserToken {}
     interface Request {
-      user?: UserToken;
+      user?: User;
     }
   }
 }
+// import type { UserToken } from "./src/interfaces/user-token";
+// import { IUser } from "./src/models/user-model";
+
+// declare global {
+//   namespace Express {
+//     interface User extends IUser {} 
+//     interface Request {
+//       user?: UserToken;
+//     }
+//   }
+// }
 
 export {};
