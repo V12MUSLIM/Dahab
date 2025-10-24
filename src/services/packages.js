573fe54ce0d8fbec4b6249d4bb92a00b94b13990
Mustafa Salem <Mustafa@gmail.com>
2025-10-24 15:26:02 +0300

    add ExperienceCard with glassmorphism design and optimize Experiences page

--- src/services/packages.js
+++ src/services/packages.js
@@ -2,5 +2,5 @@ import api from "../api/axios";
 
 export const getPackages = async () => {
   const response = await api.get("/packages");
-  return response.data;
+  return response.data.packages;
 };
