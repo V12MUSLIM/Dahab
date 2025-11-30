763b47817a0dd4aa985f9403b99152d9ba688d06
mohamedMoharek <almharyqm107@gmail.com>
2025-11-30 11:29:03 +0200

    edited

--- Dahab/src/auth/login-controller.ts
+++ Dahab/src/auth/login-controller.ts
@@ -24,45 +24,43 @@ export const loginHandler: RequestHandler = async (req, res, next) => {
         if (!email || !password) {
             return next({
                 status: 400,
-                message: "Missing credentials"});
+                message: "Missing credentials"
+            });
         }
-            // return res.status(400).json({ message: "Missing credentials" });
 
         const user = await User.findOne({ email }).select("+password").exec();
         if (!user || !user.password) {
-            // return res.status(400).json({ message: "Invalid email or password" });
-        return next({
-                status: 401,
-                message: "Invalid email or password"
-            });
-        }
-        const validPassword = await bcrypt.compare(password, user.password);
-        if (!validPassword)  {
             return next({
                 status: 401,
                 message: "Invalid email or password"
             });
         }
-            // return res.status(400).json({ message: "Invalid email or password" });
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            return next({
+                status: 401,
+                message: "Invalid email or password"
+            });
+        }
 
         const token = jwtService.createToken({ id: user._id, email: user.email, role: user.role }, { expiresIn: "2h" });
         const refreshToken = jwtService.createToken(
             { id: user._id },
             { expiresIn: "7d" }
-);
+        );
 
         res.cookie("token", token, {
             httpOnly: true,
-            sameSite: "strict",
+            sameSite: process.env.NODE_ENV === "production" ? "strict" : "none",
             secure: process.env.NODE_ENV === "production",
             maxAge: 2 * 60 * 60 * 1000,
         });
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
-            sameSite: "strict",
+            sameSite: process.env.NODE_ENV === "production" ? "strict" : "none",
             secure: process.env.NODE_ENV === "production",
-            maxAge: 7 * 24 * 60 * 60 * 1000, 
-});
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
         return res.json({ message: "Logged in successfully" });
     } catch (err) {
         next(err);
