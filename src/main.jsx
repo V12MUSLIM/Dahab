8f63416883321a680df05e3dfcf3632aa062dc71
V12MUSLIM <v12gamerboy2004@gmail.com>
2025-12-12 16:32:37 +0200

    changes on dst

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import QueryProvider from "./context/QueryProvider";
+import QueryProvider from "./Context/QueryProvider";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryProvider>
